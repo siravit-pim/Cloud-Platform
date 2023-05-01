b10151d7720481f8ba781de75fc096116054f745
Siravit Pimaksorn <120651969+siravit-pim@users.noreply.github.com>
2023-05-01 10:30:19 +0700

    Create create_bucket.sh

---
+++ GCS/create_bucket.sh
@@ -0,0 +1,17 @@
+# gsutill CheatSheet: https://bit.ly/gsutil-gist
+# ref GCS build bucket: https://cloud.google.com/storage/docs/creating-buckets#storage-create-bucket-cli
+
+# build bucket by terminal ** but setup anythinghs by default
+gsutill mb gs://r2de-test-bucket
+
+# download data
+wget -O data.zip https://file.designil.com/bhXYol+
+
+# unzip data ** data still in cloud shell
+unzip data.zip
+
+# upload data to bucket
+gsutil cp ws2-output.csv gs://r2de_datalake_test
+
+# download python
+pip3 install google-cloud-storge
